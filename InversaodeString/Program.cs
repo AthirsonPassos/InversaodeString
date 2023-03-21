ac08aee839cbdd8d7dcc2f74b89aa3c95884f294
Athirson Passos <64171655+AthirsonPassos@users.noreply.github.com>
2023-03-21 13:33:11 -0300

    Adicionar arquivos de projeto.

---
+++ InversaodeString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace InversaodeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string r = "", invertido = "";
+            Console.WriteLine("Digite uma palavra: ");
+            r = Console.ReadLine();
+            for (int i = r.Length - 1; i >= 0; i--)
+                        {
+                            invertido += r[i];
+
+                        }
+
+            Console.WriteLine("Palavra Correta: {0}", r);
+            Console.WriteLine("Palavra Invertida: {0}", invertido);
+            Console.ReadKey();
+        }
+    }
+}
